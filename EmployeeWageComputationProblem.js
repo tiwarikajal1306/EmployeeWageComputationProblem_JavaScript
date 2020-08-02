05bf5d925760a14f8f7512c757584fd02c7b35c1
kajal <tiwarikajal1306@gmail.com>
2020-08-03 01:44:43 +0530

    ADDED Function to calculate total employee wage

--- EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem.js
@@ -10,25 +10,28 @@ let totalEmpHrs = 0;
 let totalWage = 0;
 let totalWorkingDays = 0;
 
+function getWorkingHours (employee_Check) {
+ switch(employee_Check) {
+
+                case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                default:
+                        empHrs = 0;
+                }
+	return empHrs;
+}
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUMBER_OF_WORKING_DAYS) {
-	 totalWorkingDays++;
-	let employee_Check = Math.floor( Math.random() * 10 ) % 3;
-
-	switch(employee_Check) {
-
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-
-		default:
-			empHrs = 0;
-		}
-
-		totalEmpHrs += empHrs;
+	totalWorkingDays++;
+       	let employee_Check = Math.floor( Math.random() * 10 ) % 3;
+	empHrs = getWorkingHours(employee_Check);
+	totalEmpHrs += empHrs;
 }
 totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 console.log("Employee wage is" + " " +totalEmpWage);
