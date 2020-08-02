81309a72ffd988482f1e2c6a567973123e4dc060
kajal <tiwarikajal1306@gmail.com>
2020-08-02 23:53:16 +0530

    ADDED Part time employee wage

--- EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem.js
@@ -1,12 +1,17 @@
 console.log( "Welcome to Employee Wage Computation Problem" )
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
+
 let empHrs = 0;
 let empWage = 0;
-let is_Present = 1;
-let employee_Check = Math.floor( Math.random() * 10 ) % 2;
-if( employee_Check == 1 )
+
+let employee_Check = Math.floor( Math.random() * 10 ) % 3;
+if( employee_Check == IS_PART_TIME )
+	empHrs = 4;
+else if( employee_Check == IS_FULL_TIME )
 	empHrs = 8;
 else
 	empHrs = 0;
 empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log(empWage);
+console.log("Employee wage is" + " " +empWage);
