8e14b1eed4f4a26450f4c220fd8c8209f492f1c3
kajal <tiwarikajal1306@gmail.com>
2020-08-02 23:20:08 +0530

    ADDED Calculated employee wage

--- EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem.js
@@ -1,7 +1,12 @@
 console.log( "Welcome to Employee Wage Computation Problem" )
+let EMP_RATE_PER_HOUR = 20;
+let empHrs = 0;
+let empWage = 0;
 let is_Present = 1;
 let employee_Check = Math.floor( Math.random() * 10 ) % 2;
 if( employee_Check == 1 )
-	console.log( "Employee is present" );
+	empHrs = 8;
 else
-	console.log( "Employee is absent" );
+	empHrs = 0;
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log(empWage);
