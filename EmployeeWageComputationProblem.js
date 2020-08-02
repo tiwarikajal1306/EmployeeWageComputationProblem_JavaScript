284d214bae1fcf0963effc365c404505dcc61eec
kajal <tiwarikajal1306@gmail.com>
2020-08-02 22:29:24 +0530

    ADDED Check employee is present or absent

--- EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem.js
@@ -1,1 +1,7 @@
-console.log("Welcome to Employee Wage Computation Problem")
+console.log( "Welcome to Employee Wage Computation Problem" )
+let is_Present = 1;
+let employee_Check = Math.floor( Math.random() * 10 ) % 2;
+if( employee_Check == 1 )
+	console.log( "Employee is present" );
+else
+	console.log( "Employee is absent" );
