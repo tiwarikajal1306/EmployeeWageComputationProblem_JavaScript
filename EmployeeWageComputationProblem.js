7c27c41f85e291c7cf6166f42c04283cfa9e9e08
kajal <tiwarikajal1306@gmail.com>
2020-08-03 01:22:21 +0530

    ADDED Calculated employee wage till maximum hours and total working days condition

--- EmployeeWageComputationProblem.js
+++ EmployeeWageComputationProblem.js
@@ -3,29 +3,32 @@ let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
 let NUMBER_OF_WORKING_DAYS = 20;
+let MAX_HRS_IN_MONTH = 100;
 
-let empHrs = 0;
 let empWage = 0;
+let totalEmpHrs = 0;
 let totalWage = 0;
+let totalWorkingDays = 0;
 
-for (let day = 0; day < NUMBER_OF_WORKING_DAYS; day++) {
-let employee_Check = Math.floor( Math.random() * 10 ) % 3;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUMBER_OF_WORKING_DAYS) {
+	 totalWorkingDays++;
+	let employee_Check = Math.floor( Math.random() * 10 ) % 3;
 
-switch(employee_Check) {
+	switch(employee_Check) {
 
-	case IS_PART_TIME:
-		empHrs = 4;
-		break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
 
-	case IS_FULL_TIME:
-		empHrs = 8;
-		break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
 
-	default:
-		empHrs = 0;
-	}
+		default:
+			empHrs = 0;
+		}
 
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	totalWage += empWage;
+		totalEmpHrs += empHrs;
 }
-console.log("Employee wage is" + " " +totalWage);
+totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log("Employee wage is" + " " +totalEmpWage);
